ebbf0efa1214e472fed36d0a667701215b84e100
Pedro Madrigal <dnaseiseq@gmail.com>
2016-04-20 16:37:14 +0100

    Update test_fCCAC.R

--- test_fCCAC.R
+++ test_fCCAC.R
@@ -1,6 +1,4 @@
-# test_fCCAr.R
 source("fCCAC.R")
-
 bigwigs <- c("H3K4me3_act_1.bw", "H3K4me3_act_2.bw", "H3K4me3_act_3.bw" )  
 labels <- c( "H3K4me3","H3K4me3","H3K4me3" )	
 fCCAC(peaks="noY_chr_merged_act_K4.bed", bigwigs=bigwigs, labels=labels , splines=15, nbins=100, ncan=15, bar=NULL, main="H3K4me3 peaks")
